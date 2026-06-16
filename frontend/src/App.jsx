3187f671c667942157cdfef7a20989bf3a1cf297
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-06-16 22:30:32 +0530

    changes based on card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,35 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import AlgorithmCard from "./components/AlgorithmCard";
 
+function App() {
+  const [algorithms, setAlgorithms] = useState([]);
 
-function App(){
-  const [algorithms,setAlgorithms] = useState([]);
-  useEffect(()=>{
-    axios.get(
-      "http://localhost:8080/api/algorithms"
-    )
-    .then(res=>{
-      setAlgorithms(res.data);
-    })
-    .catch(err=>{
-      console.log(err);
-    })
-  },[]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/algorithms")
+      .then((res) => {
+        setAlgorithms(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
       <h1> Code Reactor</h1>
-      {algorithms.map((algo,index)=>(
-        <div key={index}>
-          <h2>{algo.name}</h2>
-          <p>
-            Category: {algo.category}
-          </p>
-          <p>
-            Time: {algo.timeComplexity}
-          </p>
-          <p>
-            Space: {algo.spaceComplexity}
-          </p>
-          <hr/>
-        </div>
-      ))}
+
+      <div className="container">
+        {algorithms.map((algo, index) => (
+          <AlgorithmCard
+            key={index}
+            algorithm={algo}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
