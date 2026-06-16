e9b2f37519bf5f1358353caced1cdaae408843a9
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-06-16 22:13:13 +0530

    minor changes

--- backend/src/main/java/com/example/demo/controller/AlgorithmController.java
+++ backend/src/main/java/com/example/demo/controller/AlgorithmController.java
@@ -1,14 +1,46 @@
 package com.example.demo.controller;
 
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.Algorithm;
+
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:5173")
 public class AlgorithmController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello fellow mate";
+
+    @GetMapping("/algorithms")
+    public List<Algorithm> getAlgorithms(){
+
+
+        return List.of(
+
+            new Algorithm(
+                "Bubble Sort",
+                "Sorting",
+                "O(n²)",
+                "O(1)"
+            ),
+
+            new Algorithm(
+                "Merge Sort",
+                "Sorting",
+                "O(n log n)",
+                "O(n)"
+            ),
+
+            new Algorithm(
+                "Binary Search",
+                "Searching",
+                "O(log n)",
+                "O(1)"
+            )
+
+        );
     }
 }
