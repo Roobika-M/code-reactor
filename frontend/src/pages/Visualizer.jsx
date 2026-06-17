0845b943ae4ad72f848fafc756282a9b21f3d03f
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-06-17 16:14:57 +0530

    bubble sort is sorted

--- frontend/src/pages/Visualizer.jsx
+++ frontend/src/pages/Visualizer.jsx
@@ -1,34 +1,105 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
 
-function Visualizer(){
+function Visualizer() {
 
-  const { name } = useParams();
+  const [array, setArray] = useState([5, 2, 8, 1]);
+  const [step, setStep] = useState(0);
+  const [running, setRunning] = useState(false);
+
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+
+  async function startSort() {
+
+    setRunning(true);
+
+    let arr = [...array];
+
+    for(let i = 0; i < arr.length; i++) {
+
+      for(let j = 0; j < arr.length - i - 1; j++) {
+
+        setStep(
+          `Comparing ${arr[j]} and ${arr[j+1]}`
+        );
+
+        await sleep(1000);
+
+
+        if(arr[j] > arr[j+1]) {
+
+          setStep(
+            `Swapping ${arr[j]} and ${arr[j+1]}`
+          );
+
+
+          let temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+
+
+          setArray([...arr]);
+
+          await sleep(1000);
+        }
+      }
+    }
+
+
+    setStep("Completed ⚡");
+    setRunning(false);
+  }
+
 
   return (
     <div>
 
       <h1>
-        ⚡ {name} Visualizer
+        ⚡ Bubble Sort Visualizer
       </h1>
 
 
       <div className="array">
 
-        <span>5</span>
-        <span>2</span>
-        <span>8</span>
-        <span>1</span>
+        {array.map((num,index)=>(
+
+          <span key={index}>
+            {num}
+          </span>
+
+        ))}
 
       </div>
 
 
       <h3>
-        Time Complexity: O(n²)
+        {step}
       </h3>
+
+
+      <button
+        onClick={startSort}
+        disabled={running}
+      >
+        Start
+      </button>
+
+
+      <p>
+        Time Complexity: O(n²)
+      </p>
+
+      <p>
+        Space Complexity: O(1)
+      </p>
+
 
     </div>
   );
-
 }
+
 
 export default Visualizer;
