58fede405edd6187a864558c517c2dfa54af85f1
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-06-17 16:06:05 +0530

    addition of pages-visualizer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,72 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import AlgorithmCard from "./components/AlgorithmCard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import AlgorithmCard from "./components/AlgorithmCard";
+import Visualizer from "./pages/Visualizer";
+
+
+function Home() {
+
   const [algorithms, setAlgorithms] = useState([]);
 
   useEffect(() => {
+
     axios
       .get("http://localhost:8080/api/algorithms")
-      .then((res) => {
+      .then(res => {
         setAlgorithms(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
       });
+
   }, []);
+
 
   return (
     <div>
-      <h1> Code Reactor</h1>
+
+      <h1>⚡ Code Reactor</h1>
 
       <div className="container">
-        {algorithms.map((algo, index) => (
+
+        {algorithms.map((algo,index)=>(
+
           <AlgorithmCard
             key={index}
             algorithm={algo}
           />
+
         ))}
+
       </div>
+
     </div>
   );
 }
+
+
+
+function App(){
+
+return (
+
+<BrowserRouter>
+
+<Routes>
+
+<Route path="/" element={<Home/>}/>
+
+<Route 
+path="/visualizer"
+element={<Visualizer/>}
+/>
+
+</Routes>
+
+</BrowserRouter>
+
+)
+
+}
+
 
 export default App;
---
+++ frontend/src/pages/Visualizer.jsx
@@ -0,0 +1,24 @@
+function Visualizer() {
+
+  return (
+    <div>
+      <h1>Algorithm Visualizer </h1>
+
+      <h2>Bubble Sort</h2>
+
+      <div>
+        [5] [2] [8] [1]
+      </div>
+
+      <p>
+        Time Complexity: O(n²)
+      </p>
+
+      <p>
+        Space Complexity: O(1)
+      </p>
+    </div>
+  );
+}
+
+export default Visualizer;
