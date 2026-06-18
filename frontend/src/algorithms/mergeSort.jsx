0d6360590f7e111d078a4d9e044909b321555808
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-06-18 11:28:02 +0530

    channged the place of words

--- frontend/src/algorithms/mergeSort.jsx
+++ frontend/src/algorithms/mergeSort.jsx
@@ -1,91 +1,107 @@
-function mergeSort(arr){
+function mergeSort(arr) {
 
-let steps=[];
-let array=[...arr];
+  let steps = [];
+  let array = [...arr];
 
 
-function addStep(arr,message){
+  function addStep(arr, message, highlight = []) {
 
- steps.push({
-   array:[...arr],
-   message
- });
+    steps.push({
+      array: [...arr],
+      message,
+      highlight
+    });
+
+  }
+
+
+  function sort(items) {
+
+    if (items.length <= 1)
+      return items;
+
+
+    let mid = Math.floor(items.length / 2);
+
+    let left = items.slice(0, mid);
+    let right = items.slice(mid);
+
+
+    addStep(
+      items,
+      "Splitting"
+    );
+
+
+    left = sort(left);
+    right = sort(right);
+
+
+    return merge(left, right);
+
+  }
+
+
+
+  function merge(left, right) {
+
+    let result = [];
+
+
+    while (left.length && right.length) {
+
+
+      addStep(
+        result.concat(left, right),
+        `Comparing ${left[0]} and ${right[0]}`,
+        [0, 1]
+      );
+
+
+      if (left[0] < right[0]) {
+
+        result.push(left.shift());
+
+      } else {
+
+        result.push(right.shift());
+
+      }
+
+
+      addStep(
+        result.concat(left, right),
+        "Placing element",
+        []
+      );
+
+    }
+
+
+    let merged = [
+      ...result,
+      ...left,
+      ...right
+    ];
+
+
+    addStep(
+      merged,
+      "Merged",
+      []
+    );
+
+
+    return merged;
+
+  }
+
+
+  sort(array);
+
+  return steps;
 
 }
 
-
-function sort(items){
-
-
-if(items.length<=1)
- return items;
-
-
-let mid=Math.floor(items.length/2);
-
-
-let left=items.slice(0,mid);
-let right=items.slice(mid);
-
-
-addStep(items,"Splitting");
-
-
-left=sort(left);
-right=sort(right);
-
-
-return merge(left,right);
-
-}
-
-
-
-function merge(left,right){
-
-let result=[];
-
-
-while(left.length && right.length){
-
-addStep(
- result.concat(left,right),
- `Comparing ${left[0]} and ${right[0]}`
-);
-
-
-if(left[0]<right[0])
- result.push(left.shift());
-
-else
- result.push(right.shift());
-
-
-}
-
-
-let merged=[
- ...result,
- ...left,
- ...right
-];
-
-
-addStep(
- merged,
- "Merged"
-);
-
-
-return merged;
-
-}
-
-
-sort(array);
-
-return steps;
-
-}
 
 export default mergeSort;
--- frontend/src/pages/Visualizer.jsx
+++ frontend/src/pages/Visualizer.jsx
@@ -3,52 +3,72 @@ import { useState } from "react";
 import bubbleSort from "../algorithms/bubbleSort";
 import mergeSort from "../algorithms/mergeSort";
 
-function Visualizer(){
-const location = useLocation();
-const algorithm = location.state;
-const [steps] = useState(() => {
 
-  if(algorithm.name === "Bubble Sort"){
-    return bubbleSort([5,2,8,1]);
-  }
+function Visualizer() {
+  const location = useLocation();
+  const algorithm = location.state;
+  const [steps] = useState(() => {
 
-  if(algorithm.name === "Merge Sort"){
-    return mergeSort([5,2,8,1]);
-  }
 
-  return [[5,2,8,1]];
-});
+    if (algorithm.name === "Bubble Sort") {
 
-const [current,setCurrent] = useState(0);
-return (
-<div>
-<h1>⚡ {algorithm.name}
-</h1>
+      return bubbleSort([5,2,8,1]);
 
-<div className="array">
-{
-steps[current].array.map((num,index)=>(
-<div className="box" key={index}>
-{num}
-<p>
-{steps[current].message}
-</p>
-</div>
-))
-}
-</div>
-<button
-onClick={()=>{
-if(current < steps.length-1)
-setCurrent(current+1);
-}}
->
-Next
-</button>
-<p>
-Step {current+1}/{steps.length}
-</p>
-</div>
-)
+    }
+    if (algorithm.name === "Merge Sort") {
+
+      return mergeSort([5,2,8,1]);
+
+    }
+    return [
+      {
+        array:[5,2,8,1],
+        message:"Starting",
+        highlight:[]
+      }
+    ];
+
+  });
+
+  const [current,setCurrent] = useState(0);
+  return (
+    <div>
+      <h1>
+        ⚡ {algorithm.name}
+      </h1>
+      <div className="array">
+        {
+          steps[current].array.map((num,index)=>(
+            <div
+              key={index}
+              className={
+                steps[current].highlight.includes(index)
+                ? "box active"
+                : "box"
+              }
+            >
+              {num}
+            </div>
+
+          ))
+
+        }
+      </div>
+      <h3>
+        {steps[current].message}
+      </h3>
+      <button
+        onClick={() => {
+          if(current < steps.length-1)
+            setCurrent(current+1);
+        }}
+      >
+        Next Step
+      </button>
+      <p>
+        Step {current+1} / {steps.length}
+      </p>
+    </div>
+  );
 }
 export default Visualizer;
