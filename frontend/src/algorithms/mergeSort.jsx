024e846aecdac4247e4fc9879ef068bcaa7b050c
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-06-18 11:18:22 +0530

    integrated words

--- frontend/src/algorithms/mergeSort.jsx
+++ frontend/src/algorithms/mergeSort.jsx
@@ -1,52 +1,91 @@
-function mergeSort(arr) {
+function mergeSort(arr){
 
-  let steps = [];
-
-  let array = [...arr];
-
-  function sort(items){
-
-    if(items.length <= 1)
-      return items;
-
-    let mid = Math.floor(items.length/2);
+let steps=[];
+let array=[...arr];
 
 
-    let left = items.slice(0,mid);
-    let right = items.slice(mid);
+function addStep(arr,message){
 
-    steps.push([...items]);
-
-    left = sort(left);
-    right = sort(right);
-
-    return merge(left, right);
-  }
-
-  function merge(left, right) {
-
-    let result = [];
-
-    while(left.length && right.length){
-
-      if(left[0] < right[0]){
-        result.push(left.shift());
-      }
-      else{
-        result.push(right.shift());
-      }
-
-    steps.push([...result,...left,...right])
-    }
-
-    return [...result,...left,...right];
-  }
-
-  sort(array);
-
-  return steps;
+ steps.push({
+   array:[...arr],
+   message
+ });
 
 }
 
+
+function sort(items){
+
+
+if(items.length<=1)
+ return items;
+
+
+let mid=Math.floor(items.length/2);
+
+
+let left=items.slice(0,mid);
+let right=items.slice(mid);
+
+
+addStep(items,"Splitting");
+
+
+left=sort(left);
+right=sort(right);
+
+
+return merge(left,right);
+
+}
+
+
+
+function merge(left,right){
+
+let result=[];
+
+
+while(left.length && right.length){
+
+addStep(
+ result.concat(left,right),
+ `Comparing ${left[0]} and ${right[0]}`
+);
+
+
+if(left[0]<right[0])
+ result.push(left.shift());
+
+else
+ result.push(right.shift());
+
+
+}
+
+
+let merged=[
+ ...result,
+ ...left,
+ ...right
+];
+
+
+addStep(
+ merged,
+ "Merged"
+);
+
+
+return merged;
+
+}
+
+
+sort(array);
+
+return steps;
+
+}
 
 export default mergeSort;
--- frontend/src/pages/Visualizer.jsx
+++ frontend/src/pages/Visualizer.jsx
@@ -3,84 +3,52 @@ import { useState } from "react";
 import bubbleSort from "../algorithms/bubbleSort";
 import mergeSort from "../algorithms/mergeSort";
 
-
 function Visualizer(){
-
 const location = useLocation();
-
 const algorithm = location.state;
-
-
 const [steps] = useState(() => {
 
   if(algorithm.name === "Bubble Sort"){
     return bubbleSort([5,2,8,1]);
   }
 
-
   if(algorithm.name === "Merge Sort"){
     return mergeSort([5,2,8,1]);
   }
 
-
   return [[5,2,8,1]];
-
 });
 
-
 const [current,setCurrent] = useState(0);
-
-
-
 return (
-
 <div>
-
-
-<h1>
-⚡ {algorithm.name}
+<h1>⚡ {algorithm.name}
 </h1>
 
-
 <div className="array">
-
-
 {
-steps[current].map((num,index)=>(
-
+steps[current].array.map((num,index)=>(
 <div className="box" key={index}>
 {num}
+<p>
+{steps[current].message}
+</p>
 </div>
-
 ))
 }
-
-
 </div>
-
-
 <button
 onClick={()=>{
-
 if(current < steps.length-1)
 setCurrent(current+1);
-
 }}
 >
 Next
 </button>
-
-
 <p>
 Step {current+1}/{steps.length}
 </p>
-
-
 </div>
-
 )
-
 }
-
-
 export default Visualizer;
